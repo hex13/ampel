1380b239725d95f85f70ba5b8fd1124b024dcaae
Łukasz Lityński <lukasz.litynski@gmail.com>
2024-11-24 17:34:35 +0100

    example: drawing

---
+++ examples/graphic-editor.js
@@ -0,0 +1,37 @@
+import * as A from '../src/ampel.js';
+
+const canvas = document.createElement('canvas');
+canvas.width = 1000;
+canvas.height = 1000;
+canvas.style.border = '1px solid red';
+document.body.append(canvas);
+const ctx = canvas.getContext('2d');
+
+const whenCanvas = A.fromEventTarget(canvas);
+
+const getCoords = (e) => {
+	const bounds = e.target.getBoundingClientRect();
+	return {x: e.clientX - bounds.x, y: e.clientY - bounds.y};
+};
+
+async function main() {
+	while (true) {
+		let pt1, pt2;
+		const e = await whenCanvas('pointerdown');
+		e.target.setPointerCapture(e.pointerId);
+		pt1 = getCoords(e);
+
+		while (true) {
+			const e = await Promise.any([whenCanvas('pointermove'), whenCanvas('pointerup')]);
+			if (e.type == 'pointerup') break;
+			pt2 = getCoords(e);
+			ctx.beginPath();
+			ctx.moveTo(pt1.x, pt1.y);
+			ctx.lineTo(pt2.x, pt2.y);
+			ctx.stroke();
+			pt1 = pt2;
+		}
+	}
+}
+
+main();
