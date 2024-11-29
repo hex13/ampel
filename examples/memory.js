4d4a48364387336f4d758b66200ba4077b010318
Łukasz Lityński <lukasz.litynski@gmail.com>
2024-11-29 23:16:55 +0100

    refactor memory example

--- examples/memory.js
+++ examples/memory.js
@@ -3,13 +3,27 @@ import * as A from '../src/ampel.js';
 const width = 5;
 const height = 4;
 const buttonSize = 50;
-const isButton = (el) => el.tagName.toLowerCase() == 'button';
 
-async function main() {
-	const container = document.createElement('div');
-	container.style.position = 'relative';
-	document.body.append(container);
-	const whenButton = new A.Listener(container).on;
+function init({ container, width, height, numbers }) {
+	for (let y = 0; y < height; y++) {
+		for (let x = 0; x < width; x++) {
+			const el = document.createElement('button');
+			el.style.position = 'absolute';
+			el.style.width = `${buttonSize}px`;
+			el.style.height = `${buttonSize}px`;
+			el.style.left = `${x * buttonSize}px`;
+			el.style.top = `${y * buttonSize}px`;
+			el.dataset.value = numbers.pop();
+			container.append(el);
+		}
+	}
+}
+
+async function playMemory(container) {
+	const buttonClicked = new A.Listener(container)
+		.on('click')
+		.filter((e) => e.target.tagName.toLowerCase() == 'button')
+		.map(e => e.target);
 
 	while (true) {
 		const numbers = [];
@@ -20,26 +34,14 @@ async function main() {
 
 		numbers.sort((a, b) => Math.random() - 0.5);
 		let buttonsLeft = numbers.length;
-
-		for (let y = 0; y < height; y++) {
-			for (let x = 0; x < width; x++) {
-				const el = document.createElement('button');
-				el.style.position = 'absolute';
-				el.style.width = `${buttonSize}px`;
-				el.style.height = `${buttonSize}px`;
-				el.style.left = `${x * buttonSize}px`;
-				el.style.top = `${y * buttonSize}px`;
-				el.dataset.value = numbers.pop();
-				container.append(el);
-			}
-		}
+		init({ container, width, height, numbers });
 
 		while (buttonsLeft > 0) {
-			const first = (await whenButton('click').filter(e => isButton(e.target))).target;
+			const first = await buttonClicked;
 			const firstValue = first.dataset.value;
 			first.innerText = firstValue;
 
-			const second = (await whenButton('click').filter(e => isButton(e.target) && e.target != first)).target;
+			const second = await buttonClicked.filter(el => el != first);
 			const secondValue = second.dataset.value;
 			second.innerText = secondValue;
 
@@ -58,4 +60,8 @@ async function main() {
 	}
 }
 
-main();
+const container = document.createElement('div');
+container.style.position = 'relative';
+document.body.append(container);
+
+playMemory(container);
