343521f2c6361929d0c4bbc8892ec3ecac8b893a
Łukasz Lityński <lukasz.litynski@gmail.com>
2024-11-30 01:59:00 +0100

    refactor memory: separation of concerns

--- examples/memory.js
+++ examples/memory.js
@@ -4,56 +4,41 @@ const width = 5;
 const height = 4;
 const buttonSize = 50;
 
-function init({ container, width, height, numbers }) {
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			const el = document.createElement('button');
-			el.style.position = 'absolute';
-			el.style.width = `${buttonSize}px`;
-			el.style.height = `${buttonSize}px`;
-			el.style.left = `${x * buttonSize}px`;
-			el.style.top = `${y * buttonSize}px`;
-			el.dataset.value = numbers.pop();
-			container.append(el);
-		}
-	}
-}
-
-async function playMemory(container) {
-	const buttonClicked = new A.Listener(container)
-		.on('click')
-		.filter((e) => e.target.tagName.toLowerCase() == 'button')
-		.map(e => e.target);
-
+async function playMemory({ container, render, hideCard, uncoverCard, coverCard }) {
 	while (true) {
-		const numbers = [];
-		for (let i = 0; i < width * height / 2; i++) {
-			numbers.push(i);
-			numbers.push(i);
+		const numbers = [...Array(width * height)].map((_, i) => Math.floor(i / 2));
+		numbers.sort((a, b) => Math.random() - 0.5);
+
+		let cardsLeft = numbers.length;
+		const cards = [];
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				cards.push({x, y, value: numbers.pop(), idx: cards.length });
+			}
 		}
 
-		numbers.sort((a, b) => Math.random() - 0.5);
-		let buttonsLeft = numbers.length;
-		init({ container, width, height, numbers });
+		render({ container, cards });
 
-		while (buttonsLeft > 0) {
-			const first = await buttonClicked;
-			const firstValue = first.dataset.value;
-			first.innerText = firstValue;
+		const cardClicked = new A.Listener(container)
+			.on('click')
+			.filter((e) => e.target.tagName.toLowerCase() == 'button')
+			.map(e => cards[e.target.dataset.idx]);
 
-			const second = await buttonClicked.filter(el => el != first);
-			const secondValue = second.dataset.value;
-			second.innerText = secondValue;
+		while (cardsLeft > 0) {
+			const first = await cardClicked;
+			uncoverCard(first);
+			const second = await cardClicked.filter(el => el != first);
+			uncoverCard(second);
 
 			await A.delay(500);
 
-			if (firstValue == secondValue) {
-				first.remove();
-				second.remove();
-				buttonsLeft -= 2;
+			if (first.value == second.value) {
+				hideCard(first);
+				hideCard(second);
+				cardsLeft -= 2;
 			} else {
-				first.innerText = '';
-				second.innerText = '';
+				coverCard(first);
+				coverCard(second);
 			}
 		}
 		alert("You win.")
@@ -64,4 +49,29 @@ const container = document.createElement('div');
 container.style.position = 'relative';
 document.body.append(container);
 
-playMemory(container);
+playMemory({
+	container,
+	render: ({ container, cards }) => {
+		cards.forEach(card => {
+			const el = document.createElement('button');
+			el.style.position = 'absolute';
+			el.style.width = `${buttonSize}px`;
+			el.style.height = `${buttonSize}px`;
+			el.style.left = `${card.x * buttonSize}px`;
+			el.style.top = `${card.y * buttonSize}px`;
+			el.dataset.value = card.value;
+			el.dataset.idx = card.idx;
+			card.el = el;
+			container.append(el);
+		});
+	},
+	hideCard: card => {
+		card.el.remove();
+	},
+	uncoverCard: card => {
+		card.el.innerText = card.value;
+	},
+	coverCard: card => {
+		card.el.innerText = '';
+	}
+});
